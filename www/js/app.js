cd09c8f654d60bc737ca8df4077c18e28d52f885
Peter Park <pjh0426@gmail.com>
2016-04-07 16:35:48 -0400

    creating a structure

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+define(['angularAMD', 'angular-route'], function (angularAMD) {
+  var app = angular.module("webapp", ['ngRoute']);
+
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when("/home", angularAMD.route({
+        templateUrl: 'view_home.html', controller: 'HomeCtrl', controllerUrl: 'controller/controller_home'
+      }))
+      .when("/view1", angularAMD.route({
+        templateUrl: 'view_view1.html', controller: 'View1Ctrl', controllerUrl: 'controller/controller_view1'
+      }))
+      .otherwise({redirectTo: "/home"});
+  });
+
+  return angularAMD.bootstrap(app);
+});
